5ee7e4bf248f2a41930645f9be18cbbe613cad35
祁煌 <huang.qi@hand-china.com>
2017-12-01 16:27:08 +0800

    [ADD] 添加pure bundle，初始项目可快速开发

---
+++ app/containers/app.js
@@ -0,0 +1,46 @@
+import { StackNavigator, TabNavigator, NavigationActions } from 'react-navigation';
+import Hello from '../pages/Hello';
+
+const App = StackNavigator(
+    {
+        Hello: { screen: Hello }
+    },
+    {
+        headerMode: 'screen'
+    }
+);
+
+const prevGetStateForAction = App.router.getStateForAction;
+App.router.getStateForAction = (action, state) => {
+    if (state && action.type === 'ReplaceCurrentScreen') {
+        const routes = state.routes.slice(0, state.routes.length - 1);
+        routes.push(action);
+        return {
+            ...state,
+            routes,
+            index: routes.length - 1,
+        };
+    }
+    if (state && action.type === 'BcakToCurrentScreen') {
+        function findDateInArr(arr, propertyName, value) {
+            for (var i = 0; i < arr.length; i++) {
+                if (arr[i][propertyName] == value) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        var i = findDateInArr(state.routes, 'routeName', action.routeName);
+        if (i != -1) {
+            var routes = state.routes.slice(0, i + 1);
+        }
+        return {
+            ...state,
+            routes,
+            index: routes.length - 1,
+        }
+    }
+    return prevGetStateForAction(action, state);
+};
+
+export default App;
